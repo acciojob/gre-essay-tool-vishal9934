2479d435d59965c37b6ac48a5591a45abad0c92b
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-03-03 15:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const display=document.querySelector("#evaluatedText");
+
+const counter=document.querySelector("#wordCount");
+
+
+display.addEventListener("input", () => {
+  let txt = display.value.trim();
+  counter.textContent = txt.split(/\s+/).filter((item) => item).length;
+});
